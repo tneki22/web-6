9687a8094ee6b7788c780bfe28462acf4e7c3697
TNekit2 <117635785+TNekit2@users.noreply.github.com>
2024-10-25 09:44:29 +0300

    all done

--- projects/1_hello/main.go
+++ projects/1_hello/main.go
@@ -1,7 +1,20 @@
 package main
 
-// здесь надо написать код
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello,web!")
+}
 
 func main() {
-	// и здесь тоже
+	// Регистрация обратботчика по пути /get
+	http.HandleFunc("/get", handler)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
